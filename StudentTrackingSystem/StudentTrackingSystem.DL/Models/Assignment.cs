fc725d2fd2a288cac2471999a65195c45b54afac
Islam shehta <168724894+Islamshe7ta@users.noreply.github.com>
2025-04-16 23:48:37 -0900

    islam add models

---
+++ StudentTrackingSystem/StudentTrackingSystem.DL/Models/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentTrackingSystem.DAL.Models
+{
+    class Assignment
+    {
+        
+        public int AssignmentId { get; set; }
+        public string AssignmentName { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
--- StudentTrackingSystem/StudentTrackingSystem.DL/Models/Student.cs
+++ StudentTrackingSystem/StudentTrackingSystem.DL/Models/Student.cs
@@ -4,17 +4,37 @@ namespace StudentTrackingSystem.Models
 {
     public class Student
     {
-        public int Id { get; set; }
+        public int StudentId { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        [Display(Name = "Student Name")]
+        [MaxLength(35)]
+        [MinLength(5)]
+        public string FullName { get; set; }
 
+
+        [Required]
         [Display(Name = "Date of Birth")]
         [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
-        public string Email { get; set; }
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string EmailAddress { get; set; }
 
-        public string Phone { get; set; }
+        [Display(Name = "Phone Number")]
+        [Required]
+        [RegularExpression(@"^(010|011|012|015)\d{8}$", ErrorMessage = "Invalid phone number. It must be 11 digits and start with 010, 011, 012, or 015.")]
+        public string PhoneNo { get; set; }
+
+        [Display(Name = "Address")]
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Grade")]
+        public int Grade { get; set; }
+
     }
 }
