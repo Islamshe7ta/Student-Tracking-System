27c48941af56b1fe3f0ee19361534989a8efe345
Islam shehta <168724894+Islamshe7ta@users.noreply.github.com>
2025-04-20 21:18:36 -0900

    delete

--- StudentTrackingSystem/StudentTrackingSystem.DL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentTrackingSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
